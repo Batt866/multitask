9519fd148106e370ea5abd647f6392c63b811882
Batt866 <25LP6030@nest.edu.mn>
2025-09-02 12:54:24 +0800

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,252 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [step, setStep] = useState("basic");
+  const [from, setForm] = useState({
+    firstname: "",
+    Lastname: "",
+    Username: "",
+  });
+  function Continue() {
+    setStep("step1");
+  }
+  function Continue2() {
+    setStep("step2");
+  }
+  const [errors, setErrors] = useState({});
+  function gotoNext() {
+    const newErrors = {};
+    const firstnameRegex = /^[A-Za-zÀ-ÖØ-öø-ÿ' -]+$/;
+    if (firstnameRegex.test(form.firstname)) {
+      newErrors.firstname = null;
+    } else {
+      newErrors.firstname = "";
+    }
+    const LastnameRegex = /^[A-Za-zÀ-ÖØ-öø-ÿ' -]+$/;
+    if (form.Lastname === "") {
+      newErrors.Lastname = "";
+    } else {
+      newErrors.Lastname = null;
+    }
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    setErrors(newErrors);
+    setStep("step1");
+  }
+
+  if (step === "basic") {
+    return (
+      <div>
+        <div className="w-[480px] h-[655px] bg-[#FFF] flex flex-col justify-between p-[32px] items-start rounded-[8xp]">
+          <header>
+            <img className="" src="pinecone-logo.png"></img>
+            <br></br>
+            <h1 className="text-black font-semibold text-2xl">Join Us! 😎</h1>
+            <br></br>
+            <h3 className="text-[#8E8E8E] items-center text-base mt-[-16px] font-sans">
+              Please provide all current information accurately.
+            </h3>
+
+            <div className="mt-5">
+              <h6 className="text-black ">First name *</h6>
+              <input
+                placeholder={"Placeholder"}
+                value={from.firstname}
+                onChange={(e) =>
+                  setForm({
+                    ...from,
+                    firstname: e.target.value,
+                  })
+                }
+                className="w-[392px] h-[40px] flex p-[12px] items-center rounded-[8px] border-[#8B8E95] border-2 text-black"
+              ></input>
+              <h6 className="text-black mt-5">Last name *</h6>
+              <input
+                placeholder={"Placeholder"}
+                onChange={(e) =>
+                  setForm({
+                    ...from,
+                    Lastname: e.target.value,
+                  })
+                }
+                value={from.Lastname}
+                className="w-[392px] h-[40px] flex p-[12px] items-center rounded-[8px] border-[#8B8E95] border-2 text-black"
+              ></input>
+              <h6 className="text-black mt-5">Username *</h6>
+              <input
+                placeholder={"Placeholder"}
+                onChange={(e) =>
+                  setForm({
+                    ...from,
+                    Username: e.target.value,
+                  })
+                }
+                value={from.Username}
+                className="w-[392px] h-[40px] flex p-[12px] items-center rounded-[8px] border-[#8B8E95] border-2 text-black"
+              ></input>
+            </div>
+          </header>
+          <button
+            onClick={gotoNext}
+            className=" w-[392px] h-[40px] bg-[#121316] text-white rounded-[6px]"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Continue 1/3
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      </div>
+    );
+  }
+  if (step === "step1") {
+    return (
+      <div>
+        <div className="w-[480px] h-[655px] bg-[#FFF] flex flex-col justify-between p-[32px] items-start rounded-[8xp]">
+          <header>
+            <img className="" src="pinecone-logo.png"></img>
+            <br></br>
+            <h1 className="text-black font-semibold text-2xl">Join Us! 😎</h1>
+            <br></br>
+            <h3 className="text-[#8E8E8E] items-center text-base mt-[-16px] font-sans">
+              Please provide all current information accurately.
+            </h3>
+
+            <div className="mt-5">
+              <h6 className="text-black mt-3">Email *</h6>
+              <input
+                placeholder={"Placeholder"}
+                value={from.firstname}
+                onChange={(e) =>
+                  setForm({
+                    ...from,
+                    firstname: e.target.value,
+                  })
+                }
+                className="w-[392px] h-[40px] flex p-[12px] items-center rounded-[8px] border-[#8B8E95] border-2 text-black"
+              ></input>
+              <h6 className="text-black mt-3">Phone number *</h6>
+              <input
+                placeholder={"Placeholder"}
+                onChange={(e) =>
+                  setForm({
+                    ...from,
+                    Lastname: e.target.value,
+                  })
+                }
+                value={from.Lastname}
+                className="w-[392px] h-[40px] flex p-[12px] items-center rounded-[8px] border-[#8B8E95] border-2 text-black"
+              ></input>
+              <h6 className="text-black mt-3">Password *</h6>
+              <input
+                placeholder={"Placeholder"}
+                onChange={(e) =>
+                  setForm({
+                    ...from,
+                    Username: e.target.value,
+                  })
+                }
+                value={from.Username}
+                className="w-[392px] h-[40px] flex p-[12px] items-center rounded-[8px] border-[#8B8E95] border-2 text-black"
+              ></input>
+              <h6 className="text-black mt-3">confirm password *</h6>
+              <input
+                placeholder={"Placeholder"}
+                onChange={(e) =>
+                  setForm({
+                    ...from,
+                    Username: e.target.value,
+                  })
+                }
+                value={from.Username}
+                className="w-[392px] h-[40px] flex p-[12px] items-center rounded-[8px] border-[#8B8E95] border-2 text-black"
+              ></input>
+            </div>
+          </header>
+          <div className="flex gap-2">
+            <button
+              onClick={() => {
+                setStep("basic");
+              }}
+              className=" w-[142px] h-[40px] bg-white text-black rounded-[6px] border-1"
+            >
+              Back
+            </button>
+            <button
+              onClick={Continue2}
+              className=" w-[272px] h-[40px] bg-[#121316] text-white rounded-[6px]"
+            >
+              Continue 2/3
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  if (step === "step2") {
+    return (
+      <div>
+        <div className="w-[480px] h-[655px] bg-[#FFF] flex flex-col justify-between p-[32px] items-start rounded-[8xp]">
+          <header>
+            <img className="" src="pinecone-logo.png"></img>
+            <br></br>
+            <h1 className="text-black font-semibold text-2xl">Join Us! 😎</h1>
+            <br></br>
+            <h3 className="text-[#8E8E8E] items-center text-base mt-[-16px] font-sans">
+              Please provide all current information accurately.
+            </h3>
+
+            <div className="mt-5">
+              <h6 className="text-black">First name *</h6>
+              <input
+                placeholder={"--/--/--"}
+                value={from.firstname}
+                onChange={(e) =>
+                  setForm({
+                    ...from,
+                    firstname: e.target.value,
+                  })
+                }
+                className="w-[392px] h-[40px] flex p-[12px] items-center rounded-[8px] border-[#8B8E95] border-2 text-black"
+              ></input>
+            </div>
+            <h6 className="text-black mt-3">Profile image *</h6>
+          </header>
+          <div className="flex gap-2">
+            <button
+              onClick={() => {
+                Continue("step1");
+              }}
+              className=" w-[142px] h-[40px] bg-white text-black rounded-[6px] border-1"
+            >
+              Back
+            </button>
+            <button
+              onClick={() => Continue}
+              className=" w-[272px] h-[40px] bg-[#121316] text-white rounded-[6px]"
+            >
+              Continue 3/3
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  if (step === "step3") {
+    return (
+      <div>
+        <div className="w-[480px] h-[655px] bg-[#FFF] flex flex-col justify-between p-[32px] items-start rounded-[8xp]">
+          <header>
+            <img className="" src="pinecone-logo.png"></img>
+            <br></br>
+            <h1 className="text-black font-semibold text-2xl">
+              You're All Set 🔥
+            </h1>
+            <br></br>
+            <h3 className="text-[#8E8E8E] items-center text-base mt-[-16px] font-sans">
+              We have received your submission. Thank you!
+            </h3>
+          </header>
+        </div>
+      </div>
+    );
+  }
 }
